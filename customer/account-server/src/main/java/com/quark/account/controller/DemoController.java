666f5fecaebf6b037d10bfbf02eea4a2b05627d9
chenxin5 <chenxin5@01zhuanche.com>
2019-01-31 16:42:52 +0800

    修改目录结构

---
+++ customer/account-server/src/main/java/com/quark/account/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.quark.account.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping("/hi")
+    public String home() {
+        return "Hello World! "+foo;
+    }
+}
